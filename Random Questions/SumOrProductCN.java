7bbc5a7bbe64420ebe3ae12be25f7033bf3ff599
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2025-07-02 21:28:00 +0530

    Added code for Sum Or Product Qs

---
+++ Random Questions/SumOrProductCN.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    public static long sumOrProduct(int n, int q) {
+        long mod = 1000000007;
+        long sum = 0, prod = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (q == 1) {
+                sum = sum + i;
+            } else {
+                prod = (prod * i) % mod;
+            }
+        }
+
+        return (q == 1) ? sum : prod;
+    }
+}
