e37375042eda6d9cdac22271c8947618b82a11df
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2025-08-14 12:57:55 +0530

    find 3 same consecutive no

---
+++ String/Largest_3_Same_Digit_In_String.java
@@ -0,0 +1,44 @@
+/* You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+It is a substring of num with length 3.
+It consists of only one unique digit.
+Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+Note:
+
+A substring is a contiguous sequence of characters within a string.
+There may be leading zeroes in num or a good integer.
+ 
+
+Example 1:
+
+Input: num = "6777133339"
+Output: "777"
+Explanation: There are two distinct good integers: "777" and "333".
+"777" is the largest, so we return "777".
+Example 2:
+
+Input: num = "2300019"
+Output: "000"
+Explanation: "000" is the only good integer.
+Example 3:
+
+Input: num = "42352338"
+Output: ""
+Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers. */
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        
+        String emp = "";
+        for (int i = 2; i < num.length();i++){
+            if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i - 1) == num.charAt(i - 2)){
+                String triple = num.substring(i - 2, i + 1);// stores the substring in triple
+                if (emp.compareTo(triple) < 0) { // keeps only the largest
+                    emp = triple;
+                }
+            }
+        }
+        return emp;
+    }
+}
