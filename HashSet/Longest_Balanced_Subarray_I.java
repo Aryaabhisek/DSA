effbb43b6db1e322e39aca2cba99bd2d551c2208
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-02-11 20:02:47 +0530

    find balance subarray

---
+++ HashSet/Longest_Balanced_Subarray_I.java
@@ -0,0 +1,66 @@
+/* You are given an integer array nums.
+
+A subarray is called balanced if the number of distinct even numbers in the subarray is equal to the number of distinct odd numbers.
+
+Return the length of the longest balanced subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,4,3]
+
+Output: 4
+
+Explanation:
+
+The longest balanced subarray is [2, 5, 4, 3].
+It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [5, 3]. Thus, the answer is 4.
+Example 2:
+
+Input: nums = [3,2,2,5,4]
+
+Output: 5
+
+Explanation:
+
+The longest balanced subarray is [3, 2, 2, 5, 4].
+It has 2 distinct even numbers [2, 4] and 2 distinct odd numbers [3, 5]. Thus, the answer is 5.
+Example 3:
+
+Input: nums = [1,2,3,2]
+
+Output: 3
+
+Explanation:
+
+The longest balanced subarray is [2, 3, 2].
+It has 1 distinct even number [2] and 1 distinct odd number [3]. Thus, the answer is 3. */
+
+
+
+
+
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            HashSet<Integer> even = new HashSet<>();
+            HashSet<Integer> odd = new HashSet<>();
+
+            for (int j = i; j < nums.length; j++){
+                if (nums[j] % 2 == 0){
+                    even.add(nums[j]);
+                }
+                else {
+                    odd.add(nums[j]);
+                }
+                if (even.size() == odd.size()){
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+}
